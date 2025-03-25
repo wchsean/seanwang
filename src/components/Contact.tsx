96313553f98c2b3e7c1c8afc1ad2c3a53d116698
sean wang <wchsean@gmail.com>
2025-03-25 14:57:16 +1100

    fix linkedin https

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -7,7 +7,7 @@ const Contact = () => {
     <div className="bg-black/40 max-w-md mx-auto mt-8 p-6 border border-gray-300 rounded-lg shadow-md">
       <h2 className="text-2xl font-bold mb-4 text-center">Contact Me</h2>
       <p>Email: <a href="mailto:wchsean@gmail.com" className="text-blue-600">wchsean@gmail.com</a></p>
-      <p>LinkedIn: <a href="www.linkedin.com/in/jheng-siang-wang-227292294" className="text-blue-600">www.linkedin.com/in/jheng-siang-wang-227292294</a></p>
+      <p>LinkedIn: <a href="https://www.linkedin.com/in/jheng-siang-wang-227292294" className="text-blue-600">www.linkedin.com/in/jheng-siang-wang-227292294</a></p>
 
       
     </div>
